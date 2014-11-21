5ae831064a8301a04ca64fe5eaee956c0e78b7d5
muga <muga.y.j3039@gmail.com>
2014-11-21 15:15:26 +0900

    Create OneClassFile
    - create a new class
    - this modifies specified class files
    - it allows to modify some classes as well

---
+++ OneClassFile.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javassist.*;
+
+public class OneClassFile extends ModifyClassFiles {
+
+	public OneClassFile(){
+		super();
+	}
+	public static void main(String[] args){
+		System.out.println("Modification for One Class File");
+		System.out.println("Enter package name (e.g. com.example.a)");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		OneClassFile ocf = new OneClassFile();
+		
+		try {
+			String c = br.readLine();
+			String[] cs = c.split(" ");
+			for(int i = 0; i < cs.length; i++){
+				ocf.insertCodesIntoAllMethods(cs[i]);
+			}
+			System.out.println("DONE !!");
+					
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
