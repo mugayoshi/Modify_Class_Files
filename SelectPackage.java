dc24858db924dcf7f0c410e784c3410800a83c4a
muga <muga.y.j3039@gmail.com>
2014-12-16 14:46:19 +0900

    Add Function to write a file
    - create a file and write the result to it

--- SelectPackage.java
+++ SelectPackage.java
@@ -1,9 +1,20 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.NotFoundException;
 
 
 public class SelectPackage extends ModifyClassFiles {
+	File file;
+	
 	public static void main(String[] args){
 		SelectPackage sp = new SelectPackage();
 		sp.searchDirectory(sp.directoryPath);
@@ -14,22 +25,76 @@ public class SelectPackage extends ModifyClassFiles {
 		try {
 			System.out.print("please input package name: ");
 			input_class = br.readLine();
-			for(int i = 0; i < classNum; i++){
-				String classname = sp.clsPckgNames.get(i);
-				if(classname.contains(input_class) == false){
-					continue;
+			System.out.print("Do you insert codes ?: ");
+			String ans = br.readLine();
+			if(ans.toLowerCase().contains("y")){
+				for(int i = 0; i < classNum; i++){
+					String classname = sp.clsPckgNames.get(i);
+					if(classname.contains(input_class) == false){
+						continue;
+					}
+					if(sp.avoidItself(classname)){
+						sp.insertCodesIntoAllMethods(classname);
+					}
+
 				}
-				if(sp.avoidItself(classname)){
-					sp.insertCodesIntoAllMethods(classname);
+				System.out.println("Writing Codes to Class Files has done !!");
+			}else{
+				System.out.print("Do you write this result to a text file ?: ");
+				String ans1 = br.readLine();
+				if(ans1.toLowerCase().contains("y")){
+					SimpleDateFormat simpleDate =  new SimpleDateFormat("dd:MM:HH:mm");
+					Date date = new Date();
+					String date_str = simpleDate.format(date);
+					sp.file = new File(input_class + "_" + date_str + "_SelectPck.txt");
+					for(int i = 0; i < sp.classFiles.size(); i++){
+						String classname = sp.clsPckgNames.get(i);
+						if(classname.contains(input_class) == false){
+							continue;
+						}
+						sp.writeMethods(classname);
+					}
+					System.out.println("Writing to " + sp.file.getName()+ "has Done");
 				}
-				
 			}
-			System.out.println("Writing Codes to Class Files has done !!");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 				
+	}
+	
+	public void writeMethods(String classname){
+		ClassPool cp = ClassPool.getDefault();
+		try {
+			FileWriter writer = new FileWriter(this.file, true);
+			//System.out.println("----Begin Methods of " + classname + " -----");
+			String begin = "----Begin Methods of " + classname + " -----\n";
+			writer.write(begin);
+			CtClass cc = cp.get(classname);
+			CtMethod[] methods = cc.getMethods();
+			for(int i = 0; i < methods.length; i++){
+				String methodname = methods[i].getName();
+				if(this.checkMethod(methodname)){
+					//System.out.println(methodname + " <- " +  methods[i].getLongName());
+					String str = methodname + " <- " +  methods[i].getLongName() + "\n";
+					writer.write(str);
+				}
+			}
+			String end = "----End Methods of " + classname + " -----\n";
+			writer.write(end);
+			writer.close();
+			
+		} catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Not Found Exception Occurred");
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}/*finally{
+			System.out.println("---- End of Methods of " + classname + " ----");
+		}*/
 	}
 }
--- ShowMethods.java
+++ ShowMethods.java
@@ -1,14 +1,20 @@
 import javassist.*;
 
 import java.io.*;
+import java.text.DateFormat;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class ShowMethods extends SearchClassFiles {
-
+	boolean write;
+	File file;
 	public ShowMethods(String[] args){
 		super(args);
+		this.write = false;
 	}
 	
 	public static void main(String[] args){
-		System.out.print("Input Class Name you're looking for: ");
+		System.out.print("Enter Class Name: ");
 		try{
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			String c = br.readLine();
@@ -16,10 +22,26 @@ public class ShowMethods extends SearchClassFiles {
 			ShowMethods sm = new ShowMethods(cs);
 			sm.searchClassFile(sm.directoryPath);
 			sm.showClasses();
-			
+			System.out.print("Do you wanna write this result to a text file ?: ");
+			String ans = br.readLine();
+			if(ans.toLowerCase().contains("y")){
+				sm.write = true;
+				SimpleDateFormat simpleDate =  new SimpleDateFormat("dd:MM:HH:mm");
+				Date date = new Date();
+				String date_str = simpleDate.format(date);
+				String input = new String();
+				for(int i = 0; i < cs.length; i++){
+					input += cs[i] + "_";
+				}
+				sm.file = new File(input + "_" + date_str + "_showMethods.txt");
+			}
 			for(int i = 0; i < sm.classFiles.size(); i++){
 				String classname = sm.clsPckgNames.get(i);
-				sm.showMethods(classname);
+				if(sm.write){
+					sm.writeMethods(classname);
+				}else{
+					sm.showMethods(classname);
+				}
 			}
 			System.out.println("Done !!");
 		}catch(IOException e){
@@ -28,6 +50,7 @@ public class ShowMethods extends SearchClassFiles {
 	}
 	
 	public void showMethods(String classname){
+		
 		System.out.println("----Begin Methods of " + classname + " -----");
 		ClassPool cp = ClassPool.getDefault();
 		try {
@@ -46,5 +69,37 @@ public class ShowMethods extends SearchClassFiles {
 			System.out.println("---- End of Methods of " + classname + " ----");
 		}
 		
+	}
+	public void writeMethods(String classname){
+		ClassPool cp = ClassPool.getDefault();
+		try {
+			FileWriter writer = new FileWriter(this.file, true);
+			//System.out.println("----Begin Methods of " + classname + " -----");
+			String begin = "----Begin Methods of " + classname + " -----\n";
+			writer.write(begin);
+			CtClass cc = cp.get(classname);
+			CtMethod[] methods = cc.getMethods();
+			for(int i = 0; i < methods.length; i++){
+				String methodname = methods[i].getName();
+				if(this.checkMethod(methodname)){
+					//System.out.println(methodname + " <- " +  methods[i].getLongName());
+					String str = methodname + " <- " +  methods[i].getLongName() + "\n";
+					writer.write(str);
+				}
+			}
+			String end = "----End Methods of " + classname + " -----\n";
+			writer.write(end);
+			writer.close();
+			
+		} catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Not Found Exception Occurred");
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}/*finally{
+			System.out.println("---- End of Methods of " + classname + " ----");
+		}*/
 	}
 }
